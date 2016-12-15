e255a14ff2f9f51685d22602849e990407480dce
mihoz-fe <mihozfe@gmail.com>
2016-12-15 11:59:34 +0200

    browser-synk was install & recuired

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass');
+var gulp 		= require('gulp'),
+	sass        = require('gulp-sass'),
+	browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
 	return gulp.src('scss/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('css'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'cv.zavarzin'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function(){
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('**/*.html', browserSync.reload);
+	gulp.watch('js/**/*.js', browserSync.reload);
 });
 
