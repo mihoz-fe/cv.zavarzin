2cba6c8270c74e30d57428c0b731bdd892014325
mihoz-fe <mihozfe@gmail.com>
2016-12-15 11:37:54 +0200

    gulp-sass was required

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('css'))
+});
 
